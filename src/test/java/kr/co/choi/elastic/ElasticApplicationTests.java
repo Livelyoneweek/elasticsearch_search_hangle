eacbb239420d5bbb3cfbb6f971c0d17cecc71ea7
Livelyoneweek <wlvvwhs@naver.com>
2024-08-25 15:01:10 +0900

    ⚡️ 기본세팅 진행
    기본 세팅

---
+++ src/test/java/kr/co/choi/elastic/ElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.choi.elastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
